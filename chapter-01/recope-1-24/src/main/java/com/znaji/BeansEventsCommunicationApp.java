d354e9bcfc69e5ac00ee86cc839aa1af22d93f8d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-12-09 17:43:53 +0100

    #chapter1: recipe 1-24 - Beans can intercommunicate btw them using the pub/sub design pattern - UserRegistrationEvent plays the roles of the event object. - it should extend ApplicationEvent.

---
+++ chapter-01/recope-1-24/src/main/java/com/znaji/BeansEventsCommunicationApp.java
@@ -0,0 +1,13 @@
+package com.znaji;
+
+/**
+ * Hello world!
+ *
+ */
+public class BeansEventsCommunicationApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
