a640a6bacd127283d573a40808caf954666768c1
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-12-09 17:48:58 +0100

    #chapter1: recipe 1-24 - UserRegistrationService plays the role of a publisher - impliment ApplicationEventPublisherAwar, to have access to ApplicationEventPublisher - when a user register, we can publishEvent(ApplicationEvent), to publish the event for all it's subs

---
+++ chapter-01/recope-1-24/src/main/java/com/znaji/UserRegistrationService.java
@@ -0,0 +1,21 @@
+package com.znaji;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+
+public class UserRegistrationService implements ApplicationEventPublisherAware {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    public void registerUser(String username, String email) {
+        // register user
+        System.out.println("User registered with username: " + username + " and email: " + email);
+        // publish event
+        applicationEventPublisher.publishEvent(new UserRegistrationEvent(this, username, email));
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+}
