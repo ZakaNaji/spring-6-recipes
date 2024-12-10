c549c2142fa3b2d13771a922d9ef12c22c8051e8
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-12-10 20:10:26 +0100

    #chapter1: recipe 1-6 - we can also load resources with help of: ClassPathResource(), FileSystemResource(), UrlResource() - PropertyLoaderUtils.loadProperties(Resource resource) is utility method/class to load properties from a resource

--- chapter-01/recipe-1-7/src/main/java/com/znaji/resouce/ResourceApp.java
+++ chapter-01/recipe-1-7/src/main/java/com/znaji/resouce/ResourceApp.java
@@ -1,11 +1,18 @@
 package com.znaji.resouce;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
 
 public class ResourceApp {
-    public static void main(String[] args) {
-        var ctx = new AnnotationConfigApplicationContext(ResourceConfig.class);
+    public static void main(String[] args) throws IOException {
+        //var ctx = new AnnotationConfigApplicationContext(ResourceConfig.class);
+        //var bannerLoader = ctx.getBean(BannerLoader.class);
 
-        var bannerLoader = ctx.getBean(BannerLoader.class);
+        var resource = new ClassPathResource("discounts.properties");
+        var properties = PropertiesLoaderUtils.loadProperties(resource);
+        System.out.println(properties);
     }
 }
