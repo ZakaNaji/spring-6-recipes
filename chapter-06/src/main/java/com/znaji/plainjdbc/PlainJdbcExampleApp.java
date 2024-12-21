61fe92448d7e56ce920ffc1b55a755b0e005fac5
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-12-21 19:52:25 +0100

    #chapter6: Plain Jdbc Dao: - create Datasource bean - use JDBC API to establish con, create statement, and then execute the statement.* - add the rest of the Dao crud implimentations

--- chapter-06/src/main/java/com/znaji/plainjdbc/PlainJdbcExampleApp.java
+++ chapter-06/src/main/java/com/znaji/plainjdbc/PlainJdbcExampleApp.java
@@ -9,6 +9,14 @@ public class PlainJdbcExampleApp {
         var ctx = new AnnotationConfigApplicationContext(PlainJdbcConfig.class);
 
         var vehicleDao = ctx.getBean(VehicleDao.class);
-        vehicleDao.insert(new Vehicle("CAR1", "RED", 4, 4));
+        //vehicleDao.insert(new Vehicle("CAR1", "RED", 4, 4));
+
+        System.out.println(vehicleDao.findByVehicleNo("CAR1"));
+        System.out.println("---after update----");
+        vehicleDao.update(new Vehicle("CAR1", "BLUE", 4, 4));
+        System.out.println(vehicleDao.findByVehicleNo("CAR1"));
+        System.out.println("---after delete----");
+        vehicleDao.delete(new Vehicle("CAR1", "BLUE", 4, 4));
+        System.out.println(vehicleDao.findByVehicleNo("CAR1"));
     }
 }
--- chapter-06/src/main/java/com/znaji/plainjdbc/PlainJdbcVehicleDao.java
+++ chapter-06/src/main/java/com/znaji/plainjdbc/PlainJdbcVehicleDao.java
@@ -6,12 +6,17 @@ import com.znaji.VehicleDao;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public class PlainJdbcVehicleDao implements VehicleDao {
 
-    private static final String INSERT_SQL = "INSERT INTO VEHICLE (VEHICLE_NO, COLOR, WHEEL, SEAT) VALUES (?, ?, ?, ?)";
+    private static final String INSERT_SQL = "INSERT INTO VEHICLE (COLOR, WHEEL, SEAT,VEHICLE_NO) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_SQL = "UPDATE VEHICLE SET COLOR=?,WHEEL=?,SEAT=? WHERE VEHICLE_NO=?";
+    private static final String SELECT_ALL_SQL = "SELECT * FROM VEHICLE";
+    private static final String SELECT_ONE_SQL = "SELECT * FROM VEHICLE WHERE VEHICLE_NO = ?";
+    private static final String DELETE_SQL = "DELETE FROM VEHICLE WHERE VEHICLE_NO=?";
     private final DataSource dataSource;
 
     public PlainJdbcVehicleDao(DataSource dataSource) {
@@ -23,6 +28,7 @@ public class PlainJdbcVehicleDao implements VehicleDao {
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SQL);
             preparedStatement(preparedStatement, vehicle);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -32,31 +38,75 @@ public class PlainJdbcVehicleDao implements VehicleDao {
 
     @Override
     public void update(Vehicle vehicle) {
-
+        try (var con = dataSource.getConnection()) {
+            var ps = con.prepareStatement(UPDATE_SQL);
+            preparedStatement(ps, vehicle);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(Vehicle vehicle) {
-
+        try (var con = dataSource.getConnection()) {
+            var ps = con.prepareStatement(DELETE_SQL);
+            ps.setString(1, vehicle.getVehicleNo());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Vehicle findByVehicleNo(String vehicleNo) {
-        return null;
+        try (var con = dataSource.getConnection()) {
+            var ps = con.prepareStatement(SELECT_ONE_SQL);
+            ps.setString(1, vehicleNo);
+            Vehicle vehicle = null;
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    vehicle = toVehicle(rs);
+                }
+            }
+            return vehicle;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
 
     @Override
     public List<Vehicle> findAll() {
-        return null;
+        try (var con = dataSource.getConnection()) {
+            var ps = con.prepareStatement(SELECT_ALL_SQL);
+            var vehicles = new java.util.ArrayList<Vehicle>();
+            try (var rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    vehicles.add(toVehicle(rs));
+                }
+            }
+            return vehicles;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void preparedStatement(PreparedStatement preparedStatement, Vehicle vehicle) {
+    private void preparedStatement(PreparedStatement ps, Vehicle vehicle) {
         try {
-            preparedStatement.setString(1, vehicle.getVehicleNo());
-            preparedStatement.setString(2, vehicle.getColor());
-            preparedStatement.setInt(3, vehicle.getWheel());
-            preparedStatement.setInt(4, vehicle.getSeat());
-            preparedStatement.executeUpdate();
+            ps.setString(1, vehicle.getColor());
+            ps.setInt(2, vehicle.getWheel());
+            ps.setInt(3, vehicle.getSeat());
+            ps.setString(4, vehicle.getVehicleNo());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Vehicle toVehicle(ResultSet rs) {
+        try {
+            return new Vehicle(rs.getString("VEHICLE_NO"), rs.getString("COLOR"), rs.getInt("WHEEL"), rs.getInt("SEAT"));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
